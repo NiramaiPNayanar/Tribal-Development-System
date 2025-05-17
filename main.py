cb5af70cbe3630bd8a95d82d9cf84cd66587c5ce
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-05-17 10:01:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from datetime import date
 import pprint
 from bson import ObjectId
-
+ 
 connection_string = "mongodb://localhost:27017"
 client = MongoClient(connection_string)
 database = client["Tribal_Management"]
